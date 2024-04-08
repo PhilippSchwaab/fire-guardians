d5e416372b4a2b616fd39e145d20650bf97dd83f
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-04-08 22:21:45 +0200

    AB#1213: New: Added map

--- src/FireGuardiansWebApp/ClientApp/src/app/home/home.component.ts
+++ src/FireGuardiansWebApp/ClientApp/src/app/home/home.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import {Component, OnInit} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {BehaviorSubject, catchError, firstValueFrom, lastValueFrom, map, Observable, of} from "rxjs";
+import {HttpClient, HttpClientJsonpModule, HttpClientModule} from "@angular/common/http";
+import {AsyncPipe, CommonModule, NgOptimizedImage} from "@angular/common";
+import {GoogleMap, MapMarker} from "@angular/google-maps";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, CommonModule, HttpClientModule, HttpClientJsonpModule, NgOptimizedImage, GoogleMap, MapMarker, AsyncPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  protected apiLoaded: BehaviorSubject<boolean>;
+  protected zoom = 8;
 
-  constructor() {
-    console.info('HomeComponent created')
+  constructor(private readonly httpClient: HttpClient) {
+
+    console.info('HomeComponent.constructor()');
+    this.apiLoaded = new BehaviorSubject<boolean>(false);
+  }
+
+  async ngOnInit(): Promise<void> {
+    console.info('HomeComponent.ngOnInit()');
+
+    try {
+      await lastValueFrom(this.httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyA2Q1cbgqajUzh0P9BIHufleusf3HSu9sY', 'callback'));
+      this.apiLoaded.next(true);
+    } catch (e) {
+      console.error('HomeComponent.ngOnInit() - e: ', e);
+    }
   }
 
 }
--- src/FireGuardiansWebApp/ClientApp/src/app/navigation/footer/footer.component.ts
+++ src/FireGuardiansWebApp/ClientApp/src/app/navigation/footer/footer.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatDividerModule} from "@angular/material/divider";
 import { VERSION } from '../../../environments/currentVersion';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatToolbarModule, MatDividerModule],
+  imports: [MatToolbarModule, MatDividerModule, NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
