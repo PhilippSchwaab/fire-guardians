31c4f28a243a675ff6658299c6786f9fa1d8c95a
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-04-08 13:28:13 +0200

    AB#1212: New: Adding styles

--- scripts/om-setupIdentityService.ps1
+++ scripts/om-setupIdentityService.ps1
@@ -1,17 +1,9 @@
-# Ensure that you have logged in to identity services (om_login_local.ps1)
+# Ensure that you have logged in to identity services (om-login-local.ps1, om-login-k8-staging.ps1)
 
-# Create resources and scopes for API
-octo-cli -c createapiresource -n fdaDataAPI -dn "FDA Data API" -d "Access to FDA data API using Data Service" -s "fdaDataAPI.full_access,fdaDataAPI.read_only"
-octo-cli -c createapiscope -n "fdaDataAPI.full_access" -e true -d "Full access to FDA data API using Data Service" -dn "Full Access FDA Data API"
-octo-cli -c createapiscope -n "fdaDataAPI.read_only" -e true -d "Readonly access to FDA data API using Data Service" -dn "Readonly Access FDA Data API"
+# Allow access of fire guardians app to identity service
+octo-cli -c AddAuthorizationCodeClient --clienturi https://localhost:7171/ --clientid fire-guardians-app --redirectUri "https://localhost:7171/" --name "Fire Guardians App"
+octo-cli -c AddAuthorizationCodeClient --clienturi https://localhost:44486/ --clientid fire-guardians-app-debug --redirectUri "https://localhost:44486/" --name "Fire Guardians App debug"
+octo-cli -c AddScopeToClient --clientid fire-guardians-app --name "systemAPI.full_access"
+octo-cli -c AddScopeToClient --clientid fire-guardians-app-debug --name "systemAPI.full_access"
 
-# Allow access of FDA Client
-octo-cli -c AddAuthorizationCodeClient --clienturi https://localhost:6006/ --clientid fda-webclient --redirectUri "https://localhost:6006/" --name "FDA WebClient"
-octo-cli -c AddAuthorizationCodeClient --clienturi https://localhost:44418/ --clientid fda-webclient-debug --redirectUri "https://localhost:44418/" --name "FDA Webclient debug"
-octo-cli -c AddScopeToClient --clientid fda-webclient --name "fdaDataAPI.full_access"
-octo-cli -c AddScopeToClient --clientid fda-webclient-debug --name "fdaDataAPI.full_access"
 
-# Add FDA management client
-octo-cli -c AddDeviceCodeClient -id fda-mgmt-cli -n "FDA Management CLI" -s 'utd.pwx8rdv9UAP7maf'
-octo-cli -c AddScopeToClient --clientid fda-mgmt-cli --name "fdaDataAPI.full_access"
-octo-cli -c AddScopeToClient --clientid fda-mgmt-cli --name "identityAPI.full_access"
